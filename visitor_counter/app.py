5f74a9cc6a3d29b468b1f02827eebc90724160da
Zacary Harmon <zcharmon77@gmail.com>
2025-12-25 17:03:13 -0500

    Inital commit adding completed SAM template.

---
+++ visitor_counter/app.py
@@ -0,0 +1,43 @@
+import json
+import os
+import boto3
+from botocore.exceptions import ClientError
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ["TABLE_NAME"])
+
+def lambda_handler(event, context):
+    try:
+        response = table.update_item(
+            Key={
+                "id": "visitors"
+            },
+            UpdateExpression="ADD visit_count :inc",
+            ExpressionAttributeValues={
+                ":inc": 1
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+
+        count = int(response["Attributes"]["visit_count"])
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "count": count
+            })}
+    except ClientError as e:
+        print("DynamoDB error:", e)
+
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "message": "Failed to update counter"
+            })}
+        
